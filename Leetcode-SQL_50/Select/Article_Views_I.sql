1b0143ce38b686cf17e21e6086de5b25e85dd65a
nmowens95 <nmowens95@gmail.com>
2024-05-19 13:15:20 -0500

    Added all select based leetcode questions

---
+++ Leetcode-SQL_50/Select/Article_Views_I.sql
@@ -0,0 +1,42 @@
+# Question:
+
+/*
+Table: Views
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| article_id    | int     |
+| author_id     | int     |
+| viewer_id     | int     |
+| view_date     | date    |
++---------------+---------+
+There is no primary key (column with unique values) for this table, the table may have duplicate rows.
+Each row of this table indicates that some viewer viewed an article (written by some author) on some date. 
+Note that equal author_id and viewer_id indicate the same person.
+ 
+
+Write a solution to find all the authors that viewed at least one of their own articles.
+
+Return the result table sorted by id in ascending order.
+*/
+
+# Answer
+SELECT DISTINCT author_id AS id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY id ASC
+
+# 5% faster, just for fun
+/*
+WITH matched AS (
+    SELECT
+        author_id,
+        viewer_id
+    FROM Views
+    WHERE author_id = viewer_id)
+
+SELECT DISTINCT author_id AS id
+FROM matched
+ORDER BY id ASC
+*/
---
+++ Leetcode-SQL_50/Select/Find_Customer_Referee.sql
@@ -0,0 +1,26 @@
+# Question:
+
+/*
+Table: Customer
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| referee_id  | int     |
++-------------+---------+
+In SQL, id is the primary key column for this table.
+Each row of this table indicates the id of a customer, their name, and the id of the customer who referred them.
+ 
+
+Find the names of the customer that are not referred by the customer with id = 2.
+
+Return the result table in any order.
+*/
+
+# Answer
+
+SELECT name
+FROM Customer
+WHERE COALESCE(referee_id, 0) <> 2 
